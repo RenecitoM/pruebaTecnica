9b980ef8bcc8b4372fb1cd6311002e428be2c512
Rene <practecnologia03@cec.comfenalco.edu.co>
2023-08-15 11:30:49 -0500

    ajuste responsive cartas

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,68 +1,70 @@
 <template>
     <div class="body">
-        <div class="card mb-4 col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 d-md-block d-lg-inline">
-            <div class="card-body">
-                <img src="./assets/Group2806.png" alt="" class="icono mb-2" />
-                <h5 class="title-card">Ideal for Beginner Clients</h5>
-                <p class="card-text">
-                    Increase trading volumes as new traders gain comfort by following
-                    successful traders in forex markets and beyond into stocks, options,
-                    commodities, and more…
-                </p>
+            <div class="card mb-4 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 m-1">
+                <div class="card-body">
+                    <img src="./assets/Group2806.png" alt="" class="icono mb-2 " />
+                    <h5 class="title-card">Ideal for Beginner Clients</h5>
+                    <p class="card-text">
+                        Increase trading volumes as new traders gain comfort by following
+                        successful traders in forex markets and beyond into stocks, options,
+                        commodities, and more…
+                    </p>
+                </div>
+            </div>
+            <div class="card mb-4 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 m-1">
+                <div class="card-body">
+                    <img src="./assets/Group2816.png" alt="" class="icono mb-2" />
+                    <h5 class="title-card">Low Barriers to Entry</h5>
+                    <p class="card-text">
+                        CommuniTraders software is free and clients can start with just a
+                        $100 minimum investment.
+                    </p>
+                </div>
+            </div>
+            <div class="card mb-4 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 m-1">
+                <div class="card-body">
+                    <img src="./assets/Group2817.png" alt="" class="icono mb-2" />
+                    <h5 class="title-card">Simple Interface</h5>
+                    <p class="card-text">
+                        A simple GUI provides needed features and tools but isn’t bogged
+                        down by non-core features. This allows clients to get setup quickly
+                        and start trading.
+                    </p>
+                </div>
+            </div>
+            <div class="card mb-4 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 m-1">
+                <div class="card-body">
+                    <img src="./assets/Group2819.png" alt="" class="icono mb-2" />
+                    <h5 class="title-card">No Long-Term Commitments</h5>
+                    <p class="card-text">
+                        Clients can easily signup to start, change signal providers, or stop the service. There is no
+                        long-term lockup.
+                    </p>
+                </div>
+            </div>
+            <div class="card mb-4 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 m-1">
+                <div class="card-body">
+                    <img src="./assets/Group2821.png" alt="" class="icono mb-2" />
+                    <h5 class="title-card">Funds Safety</h5>
+                    <p class="card-text">
+                        CommuniTraders benefits from the same strong regulation and banking partners standalone Tradeview
+                        clients enjoy along with transparency and negative balance protection.
+                    </p>
+                </div>
+            </div>
+            <div class="card mb-4 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 m-1">
+                <div class="card-body">
+                    <img src="./assets/Group2823.png" alt="" class="icono mb-2" />
+                    <h5 class="title-card">Tradeview Runs CommuniTrader</h5>
+                    <p class="card-text">
+                        You will experience the Tradeview service you know and love, no gimmicks or hidden fees, excellent
+                        live customer support, and your dedicated account manager.
+                    </p>
+                </div>
             </div>
         </div>
-        <div class="card mb-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-            <div class="card-body">
-                <img src="./assets/Group2816.png" alt="" class="icono mb-2" />
-                <h5 class="title-card">Low Barriers to Entry</h5>
-                <p class="card-text">
-                    CommuniTraders software is free and clients can start with just a
-                    $100 minimum investment.
-                </p>
-            </div>
-        </div>
-        <div class="card mb-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-            <div class="card-body">
-                <img src="./assets/Group2817.png" alt="" class="icono mb-2" />
-                <h5 class="title-card">Simple Interface</h5>
-                <p class="card-text">
-                    A simple GUI provides needed features and tools but isn’t bogged
-                    down by non-core features. This allows clients to get setup quickly
-                    and start trading.
-                </p>
-            </div>
-        </div>
-        <div class="card mb-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-            <div class="card-body">
-                <img src="./assets/Group2819.png" alt="" class="icono mb-2" />
-                <h5 class="title-card">No Long-Term Commitments</h5>
-                <p class="card-text">
-                    Clients can easily signup to start, change signal providers, or stop the service. There is no long-term lockup.
-                </p>
-            </div>
-        </div>
-        <div class="card mb-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-            <div class="card-body">
-                <img src="./assets/Group2821.png" alt="" class="icono mb-2" />
-                <h5 class="title-card">Funds Safety</h5>
-                <p class="card-text">
-                    CommuniTraders benefits from the same strong regulation and banking partners standalone Tradeview clients enjoy along with transparency and negative balance protection.
-                </p>
-            </div>
-        </div>
-        <div class="card mb-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-            <div class="card-body">
-                <img src="./assets/Group2823.png" alt="" class="icono mb-2" />
-                <h5 class="title-card">Tradeview Runs CommuniTrader</h5>
-                <p class="card-text">
-                    You will experience the Tradeview service you know and love, no gimmicks or hidden fees, excellent live customer support, and your dedicated account manager.
-                </p>
-            </div>
-        </div>
-    </div>
 </template>
-  
-  
+
 <style scoped>
 .body {
     display: flex;
@@ -73,15 +75,11 @@
 }
 
 .card {
-    flex-basis: calc(33.33% - 2em);
-    width: calc(33.33% - 2em);
-    padding: 1em;
     border: 1px solid #ccc;
     background-color: #fff;
     border-radius: 8px;
     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
     height: 16em;
-    display: grid;
 }
 
 .title-card {
@@ -94,5 +92,4 @@
 .card-text {
     margin: 0;
     color: #333;
-}
-</style>
+}</style>
