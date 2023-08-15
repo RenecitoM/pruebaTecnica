4fb153ca1df427a33673ca4662debf99cfc0a991
Rene Matorel <renealejandrojr@hotmail.com>
2023-08-15 03:14:38 -0500

    avances footer

--- src/components/Leading.vue
+++ src/components/Leading.vue
@@ -1,3 +1,93 @@
 <template>
-    
+    <div class="body">
+        <div class="left-side">
+            <div col-6 row>
+                <h2 class="title">
+                    Leading Edge Technology
+                </h2>
+            </div>
+            <p class="text row">
+                    Easy to use management tools, our fastest possible execution and spreads as low as 0.0.
+                </p>
+        </div>
+
+        <div class="right-side">
+
+        </div>
+
+    </div>
 </template>
+
+<style scoped>
+:root {
+    ---c1c3c9: #c1c3c9;
+    ---e2e2e2: #e2e2e2;
+    ---6e7079: #6e7079;
+    ---363636: #363636;
+    ---f2f2f2: #f2f2f2;
+    ---3c3e48: #3c3e48;
+    ---a51c20: #a51c20;
+    ---ff0000: #ff0000;
+
+    --unnamed-font-family-noto-sans: Noto Sans;
+    --unnamed-font-style-normal: normal;
+    --unnamed-font-weight-300: 300px;
+    --unnamed-font-weight-900: 900px;
+    --unnamed-font-weight-normal: normal;
+    --unnamed-font-size-25: 25px;
+    --unnamed-font-size-30: 30px;
+    --unnamed-font-size-32: 32px;
+    --unnamed-font-size-45: 45px;
+    --unnamed-character-spacing-0: 0px;
+    --unnamed-line-spacing-36: 36px;
+    --unnamed-line-spacing-43: 43px;
+    --unnamed-line-spacing-45: 45px;
+    --unnamed-line-spacing-64: 64px;
+}
+
+.left-side {
+    width: 100%;
+    height: 50vh;
+    background-color: #c1c3c9;
+}
+
+.right-side {
+    width: 45%;
+    height: 100%;
+    background-size: 100%;
+    background-position: center;
+    background-image: url("assets/Mask Group 2.png");
+}
+
+.title {
+    top: 2106px;
+    left: 343px;
+    width: 1000px;
+    height: 49px;
+    font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-900) var(--unnamed-font-size-30)/var(--unnamed-line-spacing-45) var(--unnamed-font-family-noto-sans);
+    letter-spacing: var(--unnamed-character-spacing-0);
+    color: var(---363636);
+    text-align: left;
+    font: normal normal 900 40px/45px Noto Sans, Black;
+    letter-spacing: 0px;
+    color: #000000;
+    opacity: 1;
+    padding-top: 10%;
+    padding-left: 10%;
+}
+
+.text {
+    top: 2177px;
+    left: 343px;
+    width: 556px;
+    height: 85px;
+    font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-normal) var(--unnamed-font-size-25)/var(--unnamed-line-spacing-36) var(--unnamed-font-family-noto-sans);
+    letter-spacing: var(--unnamed-character-spacing-0);
+    color: var(---363636);
+    text-align: left;
+    font: normal normal normal 25px/36px Noto Sans;
+    letter-spacing: 0px;
+    color: #363636;
+    opacity: 1;
+}
+</style>
